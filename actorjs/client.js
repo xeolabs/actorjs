7ea35d0d175bfe6ff4529a648c96cc6f00686128
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2013-01-05 01:20:40 +0100

    Tidy ups

--- actorjs/client.js
+++ actorjs/client.js
@@ -1,5 +1,5 @@
 /**
- * Client for connection with remote Human using cross-window messaging
+ * Client for connection with remote ActorJS using cross-window messaging
  * @param cfg
  * @constructor
  */
@@ -19,7 +19,7 @@ var ActorJSClient = function (cfg) {
         throw "element is not an iframe: '" + iframe + "'";
     }
 
-    var subHandles = new HumanAPI._Map(); // Subscription handle pool
+    var subHandles = new Map(); // Subscription handle pool
 
     var subs = {}; // Subscribers
 
